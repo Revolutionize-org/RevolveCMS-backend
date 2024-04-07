d5c27bc4f731c29c198a8574e05fca437574f70d
zoloft <whats@your.poison>
2024-04-07 16:32:45 -0400

    feat: remove user insert from seed

--- migrator/seeds.sql
+++ migrator/seeds.sql
@@ -4,12 +4,3 @@ INSERT INTO role(
 ) VALUES
     ('d7de28aa-5028-4bee-8361-7d630d86da54', 'admin'),
     ('d44e3f29-0ab5-40d4-b5d0-1e41c3cc59d3', 'user');
-
-INSERT INTO users(
-    id,
-    name,
-    email,
-    password_hash,
-    role_id
-) VALUES
-    ('c7e4b9d6-2b3b-4a0e-8d6d-6f5b0f9e0f9e', 'admin', 'aKu1X@example.com', 'admin', 'd7de28aa-5028-4bee-8361-7d630d86da54');
