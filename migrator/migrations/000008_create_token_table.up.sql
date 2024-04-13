018410b0495129f3413679f4f4e17e8ab92f212e
zoloft <whats@your.poison>
2024-04-13 17:33:48 -0400

    feat: remove user_id from token

--- migrator/migrations/000008_create_token_table.up.sql
+++ migrator/migrations/000008_create_token_table.up.sql
@@ -1,8 +1,5 @@
 CREATE TABLE token(
     id UUID PRIMARY KEY,
     token TEXT NOT NULL UNIQUE,
-    expires_at TIMESTAMPTZ,
-    user_id UUID NOT NULL,
-
-    CONSTRAINT fk_user_id FOREIGN KEY (user_id) REFERENCES users(id)
-)
+    expires_at TIMESTAMPTZ
+);
--- migrator/migrations/000009_create_index.down.sql
+++ migrator/migrations/000009_create_index.down.sql
@@ -2,6 +2,5 @@ DROP INDEX idx_header_website_id;
 DROP INDEX idx_footer_website_id;
 DROP INDEX idx_page_website_id;
 DROP INDEX idx_users_website_id;
-DROP INDEX idx_token_users_id;
 DROP INDEX idx_role_id;
 DROP INDEX idx_theme_id;
